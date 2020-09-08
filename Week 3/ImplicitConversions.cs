8365ecbfa4b2a4421f5de989e5760f9920d06480
Kambiz Saffarizadeh <k.saffarizadeh@gmail.com>
2020-09-08 18:50:42 -0500

    Create ImplicitConversions.cs

---
+++ Week 3/ImplicitConversions.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            /////////// Slide 8 ///////////
+
+            // Implicit conversion. A long can
+            // hold any value an int can hold, and more!
+            int num = 2147483647;
+            long bigNum = num;
+
+            int myInt = 9;
+
+            // Automatic casting: int to double
+            double myDouble1 = myInt;
+
+            Console.WriteLine(myInt);      // Outputs 9
+            Console.WriteLine(myDouble1);   // Outputs 9
+
+        }
+    }
+}
