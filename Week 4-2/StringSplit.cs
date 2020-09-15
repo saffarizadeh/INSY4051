3a4ffc880151719611d498162c17b8c8d406cb38
Kambiz Saffarizadeh <k.saffarizadeh@gmail.com>
2020-09-15 17:41:33 -0500

    Create StringSplit.cs

---
+++ Week 4-2/StringSplit.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "The quick brown fox jumps over the lazy dog";
+
+            // Don't worry! We will cover arrays soon!
+            string[] words = s1.Split(" ");
+
+            // Don't worry! We will also cover loops soon!
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+
+        }
+    }
+}
