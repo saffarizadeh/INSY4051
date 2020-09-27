92df33583fce22bc290962cb68444f25c7b22f87
Kambiz Saffarizadeh <k.saffarizadeh@gmail.com>
2020-09-27 16:03:02 -0500

    Create GuessingNumbersWithBreak.cs

---
+++ Week 6-2/GuessingNumbersWithBreak.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Generate a random number to be guessed
+            Random random = new Random();
+            int number = random.Next(0, 101);
+            
+            Console.WriteLine("Guess a magic number between 0 and 100");
+            while (true)
+            {
+                // Prompt the user to guess the number
+                Console.WriteLine("Enter your guess: ");
+                int guess = Convert.ToInt32(Console.ReadLine());
+                if (guess == number)
+                {
+                    Console.WriteLine("Yes, the number is " + number);
+                    break;
+                }
+                else if (guess > number)
+                {
+                    Console.WriteLine("Your guess is too high");
+                }
+                else
+                {
+                    Console.WriteLine("Your guess is too low");
+                }
+            } // End of loop
+        }
+
+    }
+}
