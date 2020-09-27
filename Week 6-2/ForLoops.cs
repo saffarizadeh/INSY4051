d7a6b93cf8bdef4843d15bc7a45a930ad537cb58
Kambiz Saffarizadeh <k.saffarizadeh@gmail.com>
2020-09-27 15:53:19 -0500

    Create ForLoops.cs

---
+++ Week 6-2/ForLoops.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for (i = 0; i < 100; i++)
+            {
+                Console.WriteLine("Welcome to C#!");
+            }
+        }
+
+    }
+}
