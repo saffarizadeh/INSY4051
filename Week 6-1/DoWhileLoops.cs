49aa8d695c2b6dae13bacd90cddcd1e7e68d6ce2
Kambiz Saffarizadeh <k.saffarizadeh@gmail.com>
2020-09-27 15:44:26 -0500

    Create DoWhileLoops.cs

---
+++ Week 6-1/DoWhileLoops.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            do
+            {
+                Console.WriteLine("Welcome to C#!");
+                count++;
+            } while (count < 100);
+        }
+
+    }
+}
