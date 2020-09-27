6451e802cb34479bb5507dc1fd88211c3315ce93
Kambiz Saffarizadeh <k.saffarizadeh@gmail.com>
2020-09-27 15:58:11 -0500

    Create BreakAndContinue.cs

---
+++ Week 6-2/BreakAndContinue.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Using break
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 4)
+                {
+                    break;
+                }
+                Console.WriteLine(i);
+            }
+
+            // Using continue
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 4)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+        }
+
+    }
+}
