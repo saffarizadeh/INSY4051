29daa11bf16efc145dfc583e60c248a5709f79a1
Kambiz Saffarizadeh <k.saffarizadeh@gmail.com>
2020-10-21 11:46:23 -0500

    Create ClassOne.cs

---
+++ Week 9-2/ClassOne.cs
@@ -0,0 +1,19 @@
+namespace MainNameSpace
+{
+    public class ClassOne
+    {
+        public int x;
+        internal int y;
+        private int z;
+
+        public void MethodOne()
+        {
+        }
+        internal void MethodTwo()
+        {
+        }
+        private void MethodThree()
+        {
+        }
+    }
+}
