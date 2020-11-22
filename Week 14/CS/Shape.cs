8d56ae73a80e70f5bd99e1f401e32e05ed731448
Kambiz Saffarizadeh <k.saffarizadeh@gmail.com>
2020-11-22 15:39:23 -0600

    Create Shape.cs

---
+++ Week 14/CS/Shape.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+	public class Shape {
+		public virtual String SayHello() {
+			return "Hello";
+		}
+	}
+
+}
